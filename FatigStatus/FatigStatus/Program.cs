ad1d422e8a2478008dde5450a5454ee1282c3dc3
Svyatoslav <svyatyar2@gmail.com>
2022-05-24 23:53:46 +0300

    Add FatigStatus

---
+++ FatigStatus/FatigStatus/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.WriteLine("Do you have fatig?  0 - no, 1 -yes. ");
+
+    int score = int.Parse(Console.ReadLine());
+
+    
+    string path = @"C:\Users\lvvm2\source\repos\FatigStatus\FatigStatus\evaluate_fatig.csv";
+
+    
+    DateTime now = DateTime.Now;
+    string text = $"{now.Day}-{now.Month}-{now.Year};{now.TimeOfDay};{score}\n";
+
+    //Console.WriteLine(text);
+
+    File.AppendAllText(path, text);
+}
