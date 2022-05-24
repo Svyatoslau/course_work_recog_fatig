362766eeed6f9c5f1998c2e95f61647b4b79c357
Svyatoslav <svyatyar2@gmail.com>
2022-05-24 23:54:43 +0300

    Add RactionTimer

---
+++ ReactionTimer/ReactionTimer/ReactionWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ReactionTimer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ReactionWindow : Page
+    {
+        private static double score;
+        private Random rng = new Random();
+        private DispatcherTimer timer = new DispatcherTimer();
+        private Stopwatch timerRed = new Stopwatch();
+
+        public ReactionWindow()
+        {
+            this.InitializeComponent();   
+            timer.Interval = new TimeSpan(0, 0, 0, rng.Next(1,5), 0);
+            timer.Tick += timer_Tick;
+            timer.Start();
+                
+        }
+
+       
+        private void timer_Tick(object sender, object e)
+        {
+            ReactionBackground.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            myText.Text = "TAP NOW!!!";
+            timerRed.Start();
+            timer.Stop();           
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+
+        private void background_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            score = timerRed.ElapsedMilliseconds;
+            timerRed.Stop();
+            this.Frame.Navigate(typeof(ResultWindow));                       
+        }
+
+        public static double getScore()
+        {
+            return score;
+        }
+
+    }
+}
---
+++ ReactionTimer/ReactionTimer/ResultWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ReactionTimer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ResultWindow : Page
+    {
+        public ResultWindow()
+        {
+            this.InitializeComponent();
+            if (ReactionWindow.getScore() == 0) Result.Text = "TOO SOON";
+            else
+            {
+                DateTime now = DateTime.Now;
+                var score = ReactionWindow.getScore();
+                Result.Text += score + " Miliseconds";
+                ReactionWindowSaveScore(now, score);
+            }          
+        }
+        private async void ReactionWindowSaveScore(DateTime now, double score)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            
+                StorageFile sampleFile = await storageFolder
+               .GetFileAsync("reaction_data.csv");
+                       
+            await FileIO.AppendTextAsync(sampleFile,
+                $"{now.Day}-{now.Month}-{now.Year};{now.TimeOfDay};{score}\n");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ReactionWindow));
+        }
+    }
+}
